14dfca7624c658fe8b8928d683cfd8742269a9eb
AMV_ND <anivnmusicvideo@gmail.com>
2023-11-29 18:57:31 +0700

    office_update_3

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import MangaList from './components/manga/MangaList';
-import MangaDetail from './components/manga/MangaDetail';
+import { React } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Header from './Components/Headers/Headers';
+import SideMain from './Components/SideMain/SideMain';
+// import { Main, Favorites, Manga, Chapter, Signup, Signin, Suggestion } from './Pages';
+
+// import SideMenu from './Features/Sidemenu/SideMenu';
+// import SideMain from './Components/SideMain/SideMain';
+// import Titles from './Pages/Titles/Titles';
+// import UserPage from './Pages/User/UserPage';
+// import Library from './Pages/Library/Library';
+// import MDLists from './Pages/MDLists/MDLists';
+// import CreateList from './Pages/CreateList/CreateList';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/manga" element={<MangaList />} />
-        <Route path="/manga/:id" element={<MangaDetail />} />
-      </Routes>
-    </Router>
+    <div className="antialiased bg-gray-50 dark:bg-gray-900">
+      <Router>
+      <Header />
+      <SideMain/>
+      {/* <div className="content__wrap">
+        <SideMenu options={{menuType: 'main'}}>
+          <SideMain />
+        </SideMenu>
+        <Routes>
+          <Route path='/' element={<Suggestion />} />
+          <Route path='/favorites' element={<Favorites />} />
+
+          <Route path='/singin' element={<Signin />} />
+          <Route path='/singup' element={<Signup />} />
+
+          <Route path='/titles/*' element={<Titles />} />
+          <Route path='/manga/:id' element={<Manga />} />
+          <Route path='/user/:id' element={<UserPage />} />
+          <Route path='/chapter/*' element={<Chapter />} />
+
+          <Route path='/follows' element={<Library />} />
+          <Route path='/lists' element={<MDLists />} />
+
+          <Route path='/create/list' element={<CreateList />} />
+          
+          <Route path='*' element={<Main />} />
+        </Routes>
+      </div> */}
+      </Router>
+    </div>
   );
-};
+}
 
 export default App;
